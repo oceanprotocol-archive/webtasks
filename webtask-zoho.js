e0ddc2f3cf3db8c4a052ec7e27995a6c066ef89a
Matthias Kretschmann <m@kretschmann.io>
2018-06-21 13:11:26 +0200

    handle CRM & Campaigns API

--- webtask-zoho.js
+++ webtask-zoho.js
@@ -5,16 +5,28 @@ const proxy = require('http-proxy-middleware')
 
 const server = express()
 
-const config = {
+const onProxyReq = function (proxyReq, req) {
+    proxyReq.setHeader('Authorization', `Zoho-oauthtoken ${req.webtaskContext.secrets.ZOHO_TOKEN}`)
+}
+
+const configZohoCRM = {
     target: 'https://www.zohoapis.com/crm/v2/',
+    pathRewrite: { '^/zoho/crm/': '/' },
     changeOrigin: true,
-    headers: {
-        Authorization: 'Zoho-oauthtoken hello'
-    }
+    onProxyReq
+}
+
+const configZohoCampaigns = {
+    target: 'https://campaigns.zoho.com/api/',
+    pathRewrite: { '^/zoho/campaigns/': '/' },
+    changeOrigin: true,
+    onProxyReq
 }
 
 server.use(cors())
-server.use('*', proxy(config))
+// server.use('*', proxy(config))
+server.use(proxy('/zoho/crm/**', configZohoCRM))
+server.use(proxy('/zoho/campaigns/**', configZohoCampaigns))
 
 server.listen(4430)
 
